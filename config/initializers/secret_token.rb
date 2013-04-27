634144885b88b4ad1b7f734c2dc7c1b9036a7945
treacle74 <paul.trelease@landregistry.gsi.gov.uk>
2013-04-27 03:05:40 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '5c725d3cc38886ec39eb3a016385740d7ed514e33e012e9acfd1bd8a25611948aa5032f1e804c8f4190fe7e03e8b1226662471445f8785bbe61e1e314e2d9615'
